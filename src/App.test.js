22c18d53aad456da132cc38f8f326373690bbb91
James Simoes <james@pocostud.io>
2018-06-06 22:17:23 -0400

    adding enzyme and jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from 'enzyme';
 import { App } from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  const props = { classes: {}, sample: {}, toggleDialogue: () => {} };
-  ReactDOM.render(<App {...props} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let props;
+describe("Main app", () => {
+
+  beforeEach(()=>{
+    props = {
+      classes: {},
+      sample: {}
+    };
+  })
+
+  it("renders without crashing", () => {
+    const wrapper = shallow(<App {...props} />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it("should match the snapshot", () => {
+    const wrapper = shallow(<App {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
