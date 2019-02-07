12908369468799de2decb29fa023a62410e6b077
James Simoes <james@pocostud.io>
2019-02-06 22:04:23 -0500

    remove testing comment

--- src/components/HelloEarthlings/HelloEarthlings.spec.js
+++ src/components/HelloEarthlings/HelloEarthlings.spec.js
@@ -2,9 +2,6 @@ import React from "react";
 import { shallow } from "enzyme";
 import { HelloEarthlings } from "./HelloEarthlings";
 
-// tests skipped until react latest alpha released with this PR fix
-// https://github.com/facebook/react/pull/14567
-
 let props;
 
 describe("HelloEarthlings", () => {
